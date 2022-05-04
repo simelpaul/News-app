bec3da7931776f4d5c9989fbaf93d694eae5014f
M0nicah <monicawughanga@gmail.com>
2022-05-04 09:59:23 +0300

    installed tailwind css and bootstrap, add get_sources function

--- app/request.py
+++ app/request.py
@@ -1,20 +1,10 @@
 import os
 import urllib.request, json
 from app.config import Config
-# from newsapi import NewsApiClient
 from .models import Article, Categories, Sources
-
-# api_key = app.config['NEWS_API_KEY']
-# base_url = app.config['NEWS_API_BASE_URL']
 
 api_key = None
 base_url = None
-
-# newsapi = NewsApiClient(api_key= Config.NEWS_API_KEY)
-
-# def configure_request(app):
-#     global api_key,base_url
-    
 
 api_key = os.environ.get('NEWS_API_KEY')
 
@@ -22,7 +12,7 @@ def get_all_articles():
 
     # all_articles = newsapi.get_everything(q='bitcoin',sources='bbc-news,the-verge',domains='bbc.co.uk,techcrunch.com',from_param='2022-04-18',to='2022-05-03',language='en',sort_by='relevancy')
 
-    get_articles_url = 'https://newsapi.org/v2/everything?q=us&from=2022-05-03&to=2021-05-01&sortBy=popularity&language=en&apikey=4a6f270476a3464092781657cfcaa274'
+    get_articles_url = 'https://newsapi.org/v2/everything?q=kenya&from=2022-05-03&to=2022-05-01&sortBy=popularity&language=en&apikey=4a6f270476a3464092781657cfcaa274'
 
     with urllib.request.urlopen(get_articles_url) as url:
         get_articles_data = url.read()
@@ -85,7 +75,7 @@ def process_sources_results(sources_list):
         language = source.get('language')
 
         if url:
-            new_source = Sources(id,description,url,category,country,language)
+            new_source = Sources(id,name,description,url,category,country,language)
             sources_results.append(new_source)
 
         return sources_results
--- app/views.py
+++ app/views.py
@@ -1,15 +1,22 @@
 from flask import render_template
 # import main
 from app import app 
-from .request import get_all_articles
+from .request import get_all_articles, get_all_sources
 # from models import Article
 
 @app.route('/')
 @app.route('/index')
 def index():
     articles = get_all_articles()
+    sources = get_all_sources()
     print(articles)
-    return render_template('index.html', articles=articles )
+    return render_template('index.html', articles=articles, sources=sources)
 
+# @app.route('/')
+# @app.route('/sources')
+# def sources():
+#     sources = get_all_sources()
+#     print(sources)
+#     return render_template('index.html', sources=sources )
 
 
