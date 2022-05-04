b6e076b46fa4322c70d209cd6ffdda2971df3417
M0nicah <monicawughanga@gmail.com>
2022-05-04 20:50:26 +0300

    add article test

--- app/news_test.py
+++ app/news_test.py
@@ -1,10 +1,10 @@
-# import unittest
-# from models import news
+import unittest
+from models import news
 
-# class news_test(unittest.TestCase):
-#     '''
-#     testing the behavior of class news
-#     '''
-#     def setUp(self):
-#         self.new_article = Articles("Marca", "Marca W", "US Family Claims Someone Used Apple AirTag To Track Them At Disney World")
-#         return 
+class news_test(unittest.TestCase):
+    '''
+    testing the behavior of class news
+    '''
+    def setUp(self):
+        self.new_article = Articles("Marca", "Marca W", "US Family Claims Someone Used Apple AirTag To Track Them At Disney World")
+        return 
